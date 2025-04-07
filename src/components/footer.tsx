98b6c5fc9759ba892202e571780d2f8f4458a5a9
Hamidou <hake92240@gmail.com>
2025-04-07 17:31:16 +0200

    titre

---
+++ src/components/footer.tsx
@@ -0,0 +1,23 @@
+import "./Footer.css"
+
+
+function Footer() {
+return (
+
+    <div>
+<h1>Contact</h1>
+<h2>Logo</h2>
+<h3>A propos de Nous</h3>
+
+
+    </div>
+
+
+)
+
+
+
+
+}
+
+export default Footer
